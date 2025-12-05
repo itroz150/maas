f0cb17a65959a1c45c603bf731f26ef485dc8367
itroz150 <itroz150@gmail.com>
2025-12-05 08:20:19 +0100

    Update req03.sql

--- req03.sql
+++ req03.sql
@@ -1,21 +1,31 @@
 CREATE VIEW "memorin"."zoned_devices_logs" AS
 SELECT
-    "l"."device_serial",
-    "l"."version_id",
-    "l"."zone_id",
-    "l"."latitude" AS "lat",
-    "l"."longitude" AS "long",
-    "l"."temperature" AS "temp",
-    "l"."battery_percentage" AS "battery",
-    "l"."signal_strength"
-FROM "memorin"."device_logs" AS "l", "memorin"."geographic_zones" AS "z", "memorin"."devices" AS "d", "memorin"."device_versions" AS "v"
-WHERE "l"."zone_id" = "z"."id"
-    AND "l"."device_serial" = "d"."serial_number"
-    AND "l"."version_id" = "v"."id"
-    AND "l"."temperature" BETWEEN 34 AND 45
-    AND "l"."signal_strength" BETWEEN 0 AND 5
-    AND "l"."battery_percentage" BETWEEN 0 AND 100
-    AND "l"."latitude" BETWEEN "z"."min_latitude" AND "z"."max_latitude"
-    AND "l"."longitude" BETWEEN "z"."min_longitude" AND "z"."max_longitude"
-    AND "d"."deactivated_at" IS NULL
+    "device_serial",
+    "version_id",
+    "zone_id",
+    "latitude" AS "lat",
+    "longitude" AS "long",
+    "temperature" AS "temp",
+    "battery_percentage" AS "battery",
+    "signal_strength"
+FROM "memorin"."device_logs"
+WHERE "temperature" BETWEEN 34 AND 45
+    AND "signal_strength" BETWEEN 0 AND 5
+    AND "battery_percentage" BETWEEN 0 AND 100
+    AND "device_serial" IN (
+        SELECT "serial_number"
+        FROM "memorin"."devices"
+        WHERE "deactivated_at" IS NULL
+    )
+    AND "version_id" IN (
+        SELECT "id"
+        FROM "memorin"."device_versions"
+    )
+    AND EXISTS (
+        SELECT 1
+        FROM "memorin"."geographic_zones"
+        WHERE "id" = "device_logs"."zone_id"
+            AND "device_logs"."latitude" BETWEEN "min_latitude" AND "max_latitude"
+            AND "device_logs"."longitude" BETWEEN "min_longitude" AND "max_longitude"
+    )
 WITH CHECK OPTION;
